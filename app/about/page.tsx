a09d0e0874023666a59c7e1da94354822a6a7432
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2023-07-19 02:15:48 -0700

    revised

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,23 +18,25 @@ export default async function BlogPage() {
         <p>
           I'm a visual thinker who likes creative challenges and learning new things. From
           doodling cartoons and dream houses as a toddler to building hamster cages as a middle schooler,
-          I nurtured an interest in both art and engineering that I carried into college and beyond.
-          At the University of Pennsylvania, I majored in an interdisciplinary program called Digital Media
-          Design and minored in Fine Arts with a focus in 3D modeling. During my time there, I also interned at the
-          New York Times where I researched VR technology and contributed graphics on the Immersive Storytelling Team.<br/><br/>
+          I developed an interest in art and engineering that flourished in my educational and professional experiences.
+          At the University of Pennsylvania, I majored in an interdisciplinary program called <u><a href="https://www.instagram.com/officialceojess/">Digital Media Design</a></u> and minored in Fine Arts with a
+          focus in 3D modeling. During that time, I also interned at the
+          New York Times where I researched VR technology and contributed graphics on the Immersive Storytelling Team. I
+          also had the unique opportunity to compete in the <u><a href="https://www.instagram.com/officialceojess/">Walt Disney Imaginations Design Competition</a></u> in 2018,
+          where my team presented a theme park idea for an abandoned space to company executives.<br/><br/>
           At Adobe, I've continued to apply and learn creative problem solving skills through a variety of projects.
           As a Technical Consultant, I designed and developed a web application that streamlines documentation of large customer workflows in
           Adobe Campaign Standard. As a Front-End Developer, I've created mock ups and
           developed internal web applications that simplify complex customer data processes
           in Adobe Experience Platform. I love developing simple, intuitive experiences that bring everyday value to
-          customers and am excited to take my next leap into design!<br/><br/>
+          customers and am excited to continue my design journey on upcoming projects.<br/><br/>
           <h3 className="font-bold text-2xl mb-8 tracking-tighter">fun facts</h3>
           <ul>
             <li>I draw cartoons on <u><a href="https://www.instagram.com/officialceojess/">CEO Jess</a></u>.</li>
             <li>I ran a best-selling, five-star graphic design shop on Etsy.</li>
             <li>My favorite movie is Shrek 2.</li>
           </ul><br/>
-          If you have any questions or want to connect, send me an email at pengjess2@gmail.com!:)
+          If you want to connect or collaborate on a project, send me an email at pengjess2@gmail.com!:)
         </p>
     </section>
   );
