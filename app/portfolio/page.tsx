7624038b273dd5f0a5a8fff5a6b5577b05aac6fd
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2023-09-10 10:57:16 -0700

    added anchor links

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -6,10 +6,15 @@ export default async function PortfolioPage() {
 
     return (
         <section>
+            <a rel="noopener" target="_blank" href="#ux_design_section">UX Design</a> /
+            <a rel="noopener" target="_blank" href="#graphic_design_section">Graphic Design</a>
+
+            <a id="ux_design_section"></a>
             <UXDesign/>
             <br/>
             .........
             <br/><br/><br/>
+            <a id="graphic_design_section"></a>
             <GraphicDesign/>
             <br/>
             <Footer/>
