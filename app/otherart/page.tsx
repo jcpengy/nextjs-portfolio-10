04945bed0e11c93c59447d91b783bb8a4fc942ed
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2023-09-10 15:54:13 -0700

    recent

---
+++ app/otherart/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import 'lightbox.js-react/dist/index.css'
+import {SlideshowLightbox, initLightboxJS} from 'lightbox.js-react'
+import {useEffect} from "react";
+
+const artimages = [
+    {
+        src: 'https://i.ibb.co/tBYMXNv/IMG-9649.jpg',
+        alt: 'Friends',
+    },
+    {
+        src: 'https://i.ibb.co/nwGMSRQ/IMG-9650.jpg',
+        alt: 'Central Park',
+    },
+    {
+        src: 'https://i.ibb.co/mz0GNzd/IMG-9654.jpg',
+        alt: 'Ye Ye',
+    },
+    {
+        src: 'https://i.ibb.co/RSjBBSK/IMG-9653.jpg',
+        alt: 'Nai Nai',
+    },
+    {
+        src: 'https://i.ibb.co/VmdbMft/Screen-Shot-2023-09-10-at-3-50-00-PM.png',
+        alt: 'Museum',
+    },
+    {
+        src: 'https://i.ibb.co/z8LTBwG/IMG-9656.jpg',
+        alt: 'Dog',
+    }
+]
+
+export const revalidate = 60;
+
+export default async function OtherArt() {
+
+    useEffect(() => {
+        initLightboxJS("9F1C-4A63-970C-2B0D", "individual");
+    });
+
+    return (
+        <section>
+            <h2>Other Art</h2>
+            <SlideshowLightbox className='container grid grid-cols-2 gap-2'>
+                {artimages.map((image) => (
+                    <img
+                        key={image.src}
+                        className='w-full rounded'
+                        src={image.src}
+                        alt={image.alt}
+                    />
+                ))}
+            </SlideshowLightbox>
+        </section>
+    );
+}
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,14 +1,18 @@
 import Footer from "../components/footer";
 import UXDesign from "../uxdesign/page";
 import GraphicDesign from "../graphicdesign/page";
+import OtherArt from "../otherart/page";
 
 export default async function PortfolioPage() {
 
     return (
         <section>
             <div id="sticky-header">
-                <a className="link" href="#ux_design_section"><u>UX Design</u></a><span> / </span>
+                <a className="link" href="#ux_design_section"><u>UX Design</u></a>
+                <span> / </span>
                 <a className="link" href="#graphic_design_section"><u>Graphic Design</u></a>
+                <span> / </span>
+                <a className="link" href="#graphic_design_section"><u>Other Art</u></a>
             </div>
             <a id="ux_design_section"></a>
             <UXDesign/>
@@ -18,6 +22,10 @@ export default async function PortfolioPage() {
             <a id="graphic_design_section"></a>
             <GraphicDesign/>
             <br/>
+            .........
+            <br/><br/><br/>
+            <a id="other_art_section"></a>
+            <OtherArt/>
             <Footer/>
         </section>
     );
