163ce406c2a5c8403ce34e7bbc2da11419d26d31
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2023-09-11 00:17:03 -0700

    added body

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ class Page extends React.Component<any, any>  {
                     {/*    </div>*/}
                     {/*</div>*/}
                     <div className="scooter">
-                        <img className="img-scooter" src="https://i.ibb.co/JkwwVsF/scooter.png" alt="scooter"/>
+                        <img className="img-scooter" src="https://i.ibb.co/Vj3RFW5/scooter.png" alt="scooter"/>
                     </div>
                     {/*<div className="scooter">*/}
                     {/*    <div className="scooter base"></div>*/}
