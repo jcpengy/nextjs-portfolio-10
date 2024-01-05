6f9f287f55778c4e11cab78c72d53632bb6e1f88
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2024-01-04 20:31:58 -0800

    added resume

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,7 +42,8 @@ export default async function AboutPage() {
                         I'm an SF-based Front-End Engineer at Adobe. I build internal tools
                         on the Digital Experience Customer Engineering team.<br/><br/>
                         I'm also an amateur cartoonist. I post regularly on @officialceojess.<br/><br/>
-                        <a className="link" target="_blank" href="mailto:pengjess2@gmail.com"><u>Email</u></a> / <a
+                        <a className="link" target="_blank" href="https://i.ibb.co/WkBrpdF/Jessica-Peng-Resume.jpg"><u>Resume</u></a> / <a
+                        className="link" target="_blank" href="mailto:pengjess2@gmail.com"><u>Email</u></a> / <a
                         className="link" target="_blank" href="https://www.linkedin.com/in/jess-peng/"><u>LinkedIn</u></a> / <a
                         className="link" target="_blank" href="https://www.instagram.com/officialceojess/"><u>Github</u></a><br/><br/>
                         <span id="dots">.........</span>
