aa032023a0d91cf2f65ff9d52214e757d91e69ec
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2024-01-04 20:46:26 -0800

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
             <br/><br/>
             .........
             <br/><br/>
-            <a className="link" target="_blank" href="https://i.ibb.co/WkBrpdF/Jessica-Peng-Resume.jpg"><u>Resume</u></a> / <a
-            className="link" target="_blank" href="mailto:pengjess2@gmail.com"><u>Email</u></> / <a
+            <a className="link" target="_blank" href="https://i.ibb.co/WkBrpdF/Jessica-Peng-Resume.jpg"><u>Resume</u></a> /
+            <a className="link" target="_blank" href="mailto:pengjess2@gmail.com"><u>Email</u></a> / <a
             className="link" target="_blank" href="https://www.linkedin.com/in/jess-peng/"><u>LinkedIn</u></a> / <a
             className="link" target="_blank" href="https://www.instagram.com/officialceojess/"><u>Github</u></a>
         </span>
