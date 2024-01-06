e96c208710baade04959bb04ed6297629018e5bc
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2024-01-05 16:47:21 -0800

    reorient

--- app/graphicdesign/page.tsx
+++ app/graphicdesign/page.tsx
@@ -88,55 +88,6 @@ export default async function GraphicDesign() {
     return (
         <section>
             <h2>Graphic Design</h2>
-            <h3>Etsy</h3>
-            <p>I'm a best-selling graphic designer on Etsy. I've created designs for weddings, bachelorette parties,
-                family vacations, and a variety of businesses.</p><br/>
-            <SlideshowLightbox className='container grid grid-cols-3 gap-2'>
-                {graphicdesignimages.map((image) => (
-                    <img
-                        key={image.src}
-                        className='w-full rounded'
-                        src={image.src}
-                        alt={image.alt}
-                    />
-                ))}
-            </SlideshowLightbox>
-            <br/>
-            <button id="button"><a target="_blank" href="https://behance.net/jesspdesign">More on my
-                Behance →
-            </a></button>
-            <h3>The New York Times</h3>
-            <p>As an Immersive Storytelling Intern at the New York Times, I created cover art and graphics for two high-profile breaking news pieces.
-                I also did R&D on emerging platforms and prospective articles.<br/><br/>
-            Credits: <a className="link" target="_blank" href="https://www.nytimes.com/interactive/2018/07/21/world/asia/thai-cave-rescue-ar-ul.html"><u>Step Inside the Thai Cave in Augmented Reality</u></a><br/>
-                <a className="link" target="_blank" href="https://www.nytimes.com/interactive/2018/06/24/world/middleeast/douma-syria-chemical-attack-augmented-reality-ar-ul.html?mtrref=undefined&gwh=F58C966F1C905F9A7F76DD29F76E6E44&gwt=pay&assetType=PAYWALL"><u>How We Created a Virtual Crime Scene to Investigate Syria's Chemical Attack</u></a>
-            </p><br/>
-            <img
-                key={'syria'}
-                className='w-full rounded'
-                src={nytimes[0].src}
-                alt={nytimes[0].alt}
-            />
-            <i>Cover graphic for "How We Created a Virtual Crime Scene to Investigate Syria's Chemical Attack"</i><br/><br/>
-            <img
-                key={'thai2'}
-                className='w-full rounded'
-                src={nytimes[3].src}
-                alt={nytimes[3].alt}
-            /><br/>
-            <img
-                key={'karthik'}
-                className='w-full rounded'
-                src={'https://i.ibb.co/SRg6ynW/karthik.jpg'}
-                alt={'karthik'}
-            /><br/>
-            <img
-                key={'cutout'}
-                className='w-full rounded'
-                src={'https://i.ibb.co/7bqMTzj/thaicave-cutout.png'}
-                alt={'cutout'}
-            />
-            <i>Graphics for "Step Inside the Thai Cave in Augmented Reality"</i><br/>
             <h3>Disney Imagineering</h3>
             <p>Slides and digital illustrations I created for Kessler Project, a theme park concept and finalist in Disney Imagineering's 2018 Imaginations Competition. In this
                 all-day space excursion, guests can explore abandoned landmarks potentially impacted by <a className="link" target="_blank" href="https://www.esa.int/Enabling_Support/Space_Engineering_Technology/The_Kessler_Effect_and_how_to_stop_it"><u>Kessler Syndrome</u></a>.</p><br/>
@@ -174,6 +125,55 @@ export default async function GraphicDesign() {
                 src='https://i.ibb.co/WW0XVLG/Screen-Shot-2023-08-28-at-2-18-26-PM.png'
                 alt='kessler 6'
             /><br/>
+            <h3>The New York Times</h3>
+            <p>As an Immersive Storytelling Intern at the New York Times, I created cover art and graphics for two high-profile breaking news pieces.
+                I also did R&D on emerging platforms and prospective articles.<br/><br/>
+            Credits: <a className="link" target="_blank" href="https://www.nytimes.com/interactive/2018/07/21/world/asia/thai-cave-rescue-ar-ul.html"><u>Step Inside the Thai Cave in Augmented Reality</u></a><br/>
+                <a className="link" target="_blank" href="https://www.nytimes.com/interactive/2018/06/24/world/middleeast/douma-syria-chemical-attack-augmented-reality-ar-ul.html?mtrref=undefined&gwh=F58C966F1C905F9A7F76DD29F76E6E44&gwt=pay&assetType=PAYWALL"><u>How We Created a Virtual Crime Scene to Investigate Syria's Chemical Attack</u></a>
+            </p><br/>
+            <img
+                key={'syria'}
+                className='w-full rounded'
+                src={nytimes[0].src}
+                alt={nytimes[0].alt}
+            />
+            <i>Cover graphic for "How We Created a Virtual Crime Scene to Investigate Syria's Chemical Attack"</i><br/><br/>
+            <img
+                key={'thai2'}
+                className='w-full rounded'
+                src={nytimes[3].src}
+                alt={nytimes[3].alt}
+            /><br/>
+            <img
+                key={'karthik'}
+                className='w-full rounded'
+                src={'https://i.ibb.co/SRg6ynW/karthik.jpg'}
+                alt={'karthik'}
+            /><br/>
+            <img
+                key={'cutout'}
+                className='w-full rounded'
+                src={'https://i.ibb.co/7bqMTzj/thaicave-cutout.png'}
+                alt={'cutout'}
+            />
+            <i>Graphics for "Step Inside the Thai Cave in Augmented Reality"</i><br/>
+            <h3>Etsy</h3>
+            <p>I'm a best-selling graphic designer on Etsy. I've created designs for weddings, bachelorette parties,
+                family vacations, and a variety of businesses.</p><br/>
+            <SlideshowLightbox className='container grid grid-cols-3 gap-2'>
+                {graphicdesignimages.map((image) => (
+                    <img
+                        key={image.src}
+                        className='w-full rounded'
+                        src={image.src}
+                        alt={image.alt}
+                    />
+                ))}
+            </SlideshowLightbox>
+            <br/>
+            <button id="button"><a target="_blank" href="https://behance.net/jesspdesign">More on my
+                Behance →
+            </a></button>
         </section>
     );
 }
