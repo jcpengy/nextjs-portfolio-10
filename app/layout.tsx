a2a63c38d6fd3aabf36a1648c308172ab7085931
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2024-02-25 14:13:38 -0800

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,83 +1,98 @@
 import './global.css';
 import clsx from 'clsx';
-import type { Metadata } from 'next';
+import type {Metadata} from 'next';
 import localFont from 'next/font/local';
 import Sidebar from './components/sidebar';
-import { Analytics } from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
 import Footer from "./components/footer";
 
 const graphik = localFont({
-  src: [
-    {
-      path: '../public/fonts/Lora-Medium.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Proxima-Nova-Regular.ttf',
-      weight: '700',
-      style: 'bold',
-    },
-  ],
-  variable: '--font-graphik',
-  display: 'swap',
+    src: [
+        {
+            path: '../public/fonts/Lora-Medium.ttf',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/Proxima-Nova-Regular.ttf',
+            weight: '700',
+            style: 'bold',
+        },
+    ],
+    variable: '--font-graphik',
+    display: 'swap',
+});
+
+const proximaNova = localFont({
+    src: [
+        {
+            path: '../public/fonts/Proxima-Nova-Regular.ttf',
+            weight: '700',
+            style: 'bold',
+        },
+    ],
+    variable: '--font-proxima',
+    display: 'swap',
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.io'),
-  title: {
-    default: 'Jess Peng',
-    template: '%s | Jess Peng',
-  },
-  description: 'Graphic designer & developer',
-  openGraph: {
-    title: 'Jess Peng',
-    description: 'Graphic designer & developer',
-    url: 'https://jcpengy.io',
-    siteName: 'Jess Peng',
-    locale: 'en-US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+    metadataBase: new URL('https://leerob.io'),
+    title: {
+        default: 'Jess Peng',
+        template: '%s | Jess Peng',
     },
-  },
-  twitter: {
-    title: 'Jess Peng',
-    card: 'summary_large_image',
-  },
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
-  },
+    description: 'Graphic designer & developer',
+    openGraph: {
+        title: 'Jess Peng',
+        description: 'Graphic designer & developer',
+        url: 'https://jcpengy.io',
+        siteName: 'Jess Peng',
+        locale: 'en-US',
+        type: 'website',
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
+    twitter: {
+        title: 'Jess Peng',
+        card: 'summary_large_image',
+    },
+    verification: {
+        google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
+        yandex: '14d2e73487fa6c71',
+    },
 };
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-      <html
-          lang="en"
-          className={clsx(
-              graphik.variable
-          )}
-      >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-      <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-        <Sidebar />
-        {children}
-        <Analytics />
-      </main>
-      </body>
-      </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={clsx(
+                graphik.variable
+            )}
+        >
+        <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <Sidebar
+                className={clsx(
+                    proximaNova.variable
+                )}/>
+            {children}
+            <Analytics/>
+        </main>
+        </body>
+        </html>
+    );
 }
