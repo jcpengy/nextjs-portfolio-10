6f6412d2fe2427aa8e5d8959f4002b64c6824f4d
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-03-29 17:49:14 -0700

    nav

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -7,20 +7,18 @@ export default async function PortfolioPage() {
 
     return (
         <section>
-            <div id="sticky-header">
+            {/* <div id="sticky-header">
                 <a className="link" href="#graphic_design_section"><u>Graphic Design</u></a>
                 <span> / </span>
-                {/*<a className="link" href="#other_art_section"><u>Fine Art</u></a>*/}
-                {/*<span> / </span>*/}
                 <a className="link" href="#ux_design_section"><u>UX Design</u></a>
-            </div>
-            <a id="graphic_design_section"></a>
+            </div> */}
+            {/* <a id="graphic_design_section"></a> */}
             <GraphicDesign/>
-            <br/>
+            {/* <br/>
             <p id="portfolio-page">.........</p>
             <br/><br/>
             <a id="ux_design_section"></a>
-            <UXDesign/>
+            <UXDesign/> */}
         </section>
     );
 }
