04fff9e50cd3ffa308f346e01d5a0f194f023ba4
Jess Peng <jesspeng@Jesss-Air.attlocal.net>
2024-03-31 13:51:12 -0700

    M

--- app/art/page.tsx
+++ app/art/page.tsx
@@ -12,6 +12,9 @@ import Dog1 from "../../public/images/Fine Art/dog1.jpg";
 import Dog2 from "../../public/images/Fine Art/dog2.jpg";
 import Dog3 from "../../public/images/Fine Art/dog3.jpg";
 import Card from "../../public/images/Fine Art/happyholidays.jpg";
+import Painting from "../../public/images/Fine Art/painting.jpg";
+import DogPortraits from "../../public/images/Fine Art/dog-portraits.jpg.jpg";
+import Dog4 from "../../public/images/Fine Art/dogs.jpg";
 
 export default async function Art() {
 
@@ -21,7 +24,14 @@ export default async function Art() {
 
     return (
         <section id="portfolio-page">
-            <p>View more of my personal art at <a className="link" href="https://instagram.com/jessp.design" target="_blank">@jessp.design</a>.</p><br/>
+            <p>I love drawing dogs. To request a portrait of your furry friend, please email pengjess2@gmail.com or (for smaller portraits) submit an order through <a className="link" href="https://www.etsy.com/listing/771510565/custom-pet-portrait-mini-pet-portrait?click_key=2a1f63c3f155ec69ad8321fb86f5616b4c7c2cd4%3A771510565&click_sum=0b9786a5&ref=shop_home_active_13&frs=1" target="_blank">here</a>.</p><br/>
+            <img
+                    id="art-image"
+                    key='dogportraits'
+                    className='w-full rounded'
+                    src={DogPortraits.src}
+                    alt='dogportraits'
+                />
             <div className="md:grid md:grid-cols-3">
                 <img
                     id="art-image"
@@ -37,20 +47,21 @@ export default async function Art() {
                     src={Enzo.src}
                     alt='enzo'
                 />
-                <img
-                    id="art-image"
-                    key='card'
-                    className='w-full rounded'
-                    src={Card.src}
-                    alt='card'
-                />
-                <img
+                 <img
                     id="art-image"
                     key='dog1'
                     className='w-full rounded'
                     src={Dog1.src}
                     alt='dog1'
                 />
+                <img
+                    id="art-image"
+                    key='dog4'
+                    className='w-full rounded'
+                    src={Dog4.src}
+                    alt='dog4'
+                />
+               
                 <img
                     id="art-image"
                     key='dog2'
@@ -67,10 +78,10 @@ export default async function Art() {
                 />
                 <img
                     id="art-image"
-                    key='cat'
+                    key='painting'
                     className='w-full rounded'
-                    src={Cat.src}
-                    alt='cat'
+                    src={Painting.src}
+                    alt='painting'
                 />
                 <img
                     id="art-image"
@@ -81,12 +92,20 @@ export default async function Art() {
                 />
                 <img
                     id="art-image"
+                    key='card'
+                    className='w-full rounded'
+                    src={Card.src}
+                    alt='card'
+                />
+                <img
+                    id="art-image"
                     key='centralpark'
                     className='w-full rounded'
                     src={CentralPark.src}
                     alt='centralpark'
                 />
             </div>
+            <br/>
             {/* <SlideshowLightbox className='container grid grid-cols-3 gap-2'>
                 {artimages.map((image) => (
                     <img
