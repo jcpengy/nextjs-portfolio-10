069889ff1b185c8803f5ada659819b68a08b5bfc
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-04-01 12:37:53 -0700

    M

--- app/etsy/page.tsx
+++ app/etsy/page.tsx
@@ -7,6 +7,8 @@ import Allison1 from "../../public/images/Etsy/allison-01.jpg";
 import Allison2 from "../../public/images/Etsy/allison-02.jpg";
 import EM1 from "public/images/Etsy/dana-01.jpg";
 import EM2 from "public/images/Etsy/dana-02.jpg";
+import BabyTop from "public/images/Etsy/IMG_1486.jpg";
+import BabyLeft from "public/images/Etsy/IMG_1487.jpg";
 import BabyRight from "public/images/Etsy/IMG_1488.jpg";
 import Card from "public/images/Etsy/card.jpg";
 
@@ -56,7 +58,7 @@ export default async function Etsy() {
     return (
         <section id="portfolio-page">
             <h3 id="all-caps">ETSY</h3>
-            <p>Freelance work I've done for small businesses, schools, and family
+            <p>Freelance design work I've done for small businesses, schools, and family
                 events like weddings, vacations, and reunions.<br/><br/>
                 Visit my Etsy shop <a className="link" href="https://www.etsy.com/shop/JessPDesign?ref=seller-platform-mcnav">here</a>.</p><br/>
                 <i>Robbie & Emily's Wedding Album</i>
@@ -93,17 +95,9 @@ export default async function Etsy() {
                 />
                 </div>
                 <br/>
-                <i>Goodbye Card</i>
+                <i>Baby wrap</i>
                 <img
                     id="art-image"
-                    key='goodbye'
-                    className='w-full rounded'
-                    src={Card.src}
-                    alt='goodbye'
-                /><br/>
-                {/* </div> */}
-                {/* <img
-                    id="centered-caption"
                     key='baby-top'
                     src={BabyTop.src}
                     alt='baby-top'
@@ -111,29 +105,26 @@ export default async function Etsy() {
                 <br/>
                 <div className="md:grid md:grid-cols-2">
                 <img
-                    id="centered-caption"
+                    id="art-image"
                     key='baby-left'
                     src={BabyLeft.src}
                     alt='baby-left'
                 />
                 <img
-                    id="centered-caption"
+                    id="art-image"
                     key='baby-right'
                     src={BabyRight.src}
                     alt='baby-right'
                 />
-                </div>
-                <br/><br/> */}
-            {/* <SlideshowLightbox className='container grid grid-cols-3 gap-2'>
-                {graphicdesignimages.map((image) => (
-                    <img
-                        key={image.src}
-                        className='w-full rounded'
-                        src={image.src}
-                        alt={image.alt}
-                    />
-                ))}
-            </SlideshowLightbox> */}
+                </div><br/>
+                <i>Goodbye Card</i>
+                <img
+                    id="art-image"
+                    key='goodbye'
+                    className='w-full rounded'
+                    src={Card.src}
+                    alt='goodbye'
+                />
             <br/>
             {/* <button id="button"><a target="_blank" href="https://behance.net/jesspdesign">More on my
                 Behance →
