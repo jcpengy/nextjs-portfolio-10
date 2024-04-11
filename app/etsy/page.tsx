0d17f8cc88834fee75195263a6b06c28143eeac4
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-04-10 21:46:06 -0700

    M

--- app/etsy/page.tsx
+++ app/etsy/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 import Footer from "../components/footer";
 import React from "react";
-import Allison1 from "../../public/images/Etsy/allison-01.jpg";
-import Allison2 from "../../public/images/Etsy/allison-02.jpg";
+import Allison1 from "../../public/images/Etsy/allison-04.jpg";
 import EM1 from "public/images/Etsy/dana-01.jpg";
 import EM2 from "public/images/Etsy/dana-02.jpg";
 import EM3 from "public/images/Etsy/dana-03.jpg";
@@ -16,24 +15,14 @@ export default async function Etsy() {
             <h3 id="all-caps">ETSY</h3>
             <p>Freelance design work I've done for small businesses and individual customers.<br/><br/>
                 Visit my Etsy shop <a className="link" href="https://www.etsy.com/shop/JessPDesign?ref=seller-platform-mcnav">here</a>.</p><br/>
-                <i>Robbie & Emily's wedding album</i>
                 <img
                     id="art-image"
                     key='robbieemily1'
                     className='w-full rounded'
                     src={Allison1.src}
                     alt='robbieemily1'
-                /><br/>
-                <i>Robbie & Emily's wedding records</i>
-                <img
-                    id="art-image"
-                    key='robbieemily2'
-                    className='w-full rounded'
-                    src={Allison2.src}
-                    alt='robbieemily2'
-                 />
+                />
                 <br/>
-                <i>Eid Mubarak wrapping paper for kids and adults</i>
                 <div className="md:grid md:grid-cols-3">
                 <img
                     id="art-image"
@@ -58,7 +47,6 @@ export default async function Etsy() {
                 />
                 </div>
                 <br/>
-                <i>Stickers for a homemade dog food business</i>
                 <img
                     id="art-image"
                     key='dog stickers'
@@ -67,7 +55,6 @@ export default async function Etsy() {
                     alt='dog stickers'
                 />
                 <br/>
-                <i>Goodbye card</i>
                 <img
                     id="art-image"
                     key='goodbye'
