ea5770a5d96f58efa2364f4f97bc72a13c78f468
Jess Peng <jesspeng@Jesss-Air.attlocal.net>
2024-04-14 15:31:17 -0700

    M

--- app/moreart/page.tsx
+++ app/moreart/page.tsx
@@ -3,7 +3,8 @@ import Footer from "../components/footer";
 import Beach from "../../public/images/Fine Art/beach.jpg";
 import CentralPark from "../../public/images/Fine Art/centralpark.jpg";
 import Cat from "../../public/images/Fine Art/cat.jpg";
-import Pencil from "../../public/images/Fine Art/pencil.jpg";
+import Collage1 from "../../public/images/Fine Art/collage1.jpg";
+import Collage2 from "../../public/images/Fine Art/collage2.jpg";
 import Painting from "../../public/images/Fine Art/painting.jpg";
 
 
@@ -37,25 +38,18 @@ export default async function MoreArt() {
             <div className="md:grid md:grid-cols-3">
                 <img
                     id="art-image"
-                    key='pencil'
+                    key='collage1'
                     className='w-full rounded'
-                    src={Pencil.src}
-                    alt='pencil'
-                />
-                {/* <img
-                    id="art-image"
-                    key='beach'
-                    className='w-full rounded'
-                    src={Beach.src}
-                    alt='beach'
+                    src={Collage1.src}
+                    alt='collage1'
                 />
                 <img
                     id="art-image"
-                    key='painting'
+                    key='collage2'
                     className='w-full rounded'
-                    src={Painting.src}
-                    alt='painting'
-                /> */}
+                    src={Collage2.src}
+                    alt='collage2'
+                /> 
             </div>
             <br/>
         </section>
