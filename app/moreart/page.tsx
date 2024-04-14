64bbb897aa8c2a9fde8c297ef60139c21b0d1517
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-04-14 13:38:06 -0700

    M

--- app/moreart/page.tsx
+++ app/moreart/page.tsx
@@ -3,7 +3,7 @@ import Footer from "../components/footer";
 import Beach from "../../public/images/Fine Art/beach.jpg";
 import CentralPark from "../../public/images/Fine Art/centralpark.jpg";
 import Cat from "../../public/images/Fine Art/cat.jpg";
-import Card from "../../public/images/Fine Art/happyholidays.jpg";
+import Pencil from "../../public/images/Fine Art/pencil.jpg";
 import Painting from "../../public/images/Fine Art/painting.jpg";
 
 
@@ -33,6 +33,29 @@ export default async function MoreArt() {
                     src={Painting.src}
                     alt='painting'
                 />
+            </div>
+            <div className="md:grid md:grid-cols-3">
+                <img
+                    id="art-image"
+                    key='pencil'
+                    className='w-full rounded'
+                    src={Pencil.src}
+                    alt='pencil'
+                />
+                {/* <img
+                    id="art-image"
+                    key='beach'
+                    className='w-full rounded'
+                    src={Beach.src}
+                    alt='beach'
+                />
+                <img
+                    id="art-image"
+                    key='painting'
+                    className='w-full rounded'
+                    src={Painting.src}
+                    alt='painting'
+                /> */}
             </div>
             <br/>
         </section>
