6851bc73c4f53ebc9210484df306f388f07a1e8c
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-08-28 21:00:17 -1000

    m

--- app/graphicdesign/page.tsx
+++ app/graphicdesign/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import Eid from "public/images/Eid/finalImages/eid-cover.jpg";
-import River from "public/images/RiversEdge/finalImages/riversedge-cover.jpg";
+import Eid from "public/images/Eid/finalImages/1.jpg";
+import River from "public/images/RiversEdge/riversedge-cover.jpg";
 import KesslerSlide2 from "public/images/Kessler/kessler-cover.jpg";
 import BlackPower from "public/images/BPB/bpb-cover.jpg";
 import SFGrooming from "public/images/SFGrooming/finalImages/1.jpg";
