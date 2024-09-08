893f6965e60f0486348cd9ec22b2503d716be45b
Jess Peng <jesspeng@Jesss-MacBook-Air.local>
2024-09-08 16:19:22 -0700

    M

--- app/about-blurb/page.tsx
+++ app/about-blurb/page.tsx
@@ -6,7 +6,7 @@ export default function AboutSection() {
             <div id="section-description">
                 <h2>Hello, I'm Jess.</h2>
                 <div id="section-description">
-                I'm a senior front-end engineer at Adobe in San Francisco and a design generalist. I have a background in UI/UX design and development, graphic design, and fine art. <br/><u><a href="/about">Full bio</a></u> / <u><a href="https://acrobat.adobe.com/id/urn:aaid:sc:US:d95711aa-c25b-4d6f-a5e3-7a633f4ea593" target="_blank">Resume</a></u>
+                I'm a senior front-end engineer at Adobe and a design generalist with a background in UI/UX design and development, graphic design, and fine art. <br/><u><a href="/about">Full bio</a></u> / <u><a href="https://acrobat.adobe.com/id/urn:aaid:sc:US:d95711aa-c25b-4d6f-a5e3-7a633f4ea593" target="_blank">Resume</a></u>
                 </div>
             </div>
         </section>
