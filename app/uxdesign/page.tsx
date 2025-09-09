59f3ecc98bdfa65d029cccc30ebb9e5e240c3684
Jess Peng <jesspeng@jesss-mbp.macromedia.com>
2025-09-09 11:26:19 -0700

    home

--- app/uxdesign/page.tsx
+++ app/uxdesign/page.tsx
@@ -7,68 +7,72 @@ import Disney from "../../public/images/Kessler/wendy-stars.jpg";
 import ProjectLasso from "../../public/images/ProjectLasso/m1/odl/a.png";
 
 const images = [
-    { "src": CEEM.src, "alt": "CEEM", "link": "/ceem" },
-    { "src": Sandbox.src, "alt": "Sandbox Copy", "link": "/sandboxcopy" },
-    { "src": OnDemandLearning.src, "alt": "On-Demand Learning", "link": '/on-demand-learning' },
-    { "src": ProjectLasso.src, "alt": "Project Lasso", "link": '/projectlasso' },
-    { "src": NYT.src, "alt": "The New York Times", "link": '/new-york-times' },
-    { "src": Disney.src, "alt": "Disney Imagineering", "link": '/kessler' }
+  { "src": CEEM.src, "alt": "CEEM", "link": "/ceem" },
+  { "src": Sandbox.src, "alt": "Sandbox Copy", "link": "/sandboxcopy" },
+  { "src": OnDemandLearning.src, "alt": "On-Demand Learning", "link": '/on-demand-learning' },
+  { "src": ProjectLasso.src, "alt": "Project Lasso", "link": '/projectlasso' },
+  { "src": NYT.src, "alt": "The New York Times", "link": '/new-york-times' },
+  { "src": Disney.src, "alt": "Disney Imagineering", "link": '/kessler' }
 ]
 
 export default function UXDesign() {
-    return (
-      <section>
-        <div id="center-text">
-          <h4>Featured projects</h4>
-        </div>
-        <br />
-        <br />
-        <div className="two-column-container">
-          <div className="column">
-            <a href={images[3].link}>
-              <img id="clickable-image" src={ProjectLasso.src} alt="Project Lasso"/>
-            </a>
+  return (
+    <section>
+      <div id="center-text">
+        <h4>Featured projects</h4>
+      </div>
+      <br />
+      <br />
+      <div className="two-column-container">
+        <div className="column">
+          <a href={images[3].link}>
+            <img id="clickable-image" src={ProjectLasso.src} alt="Project Lasso"/>
+          </a>
+          <div className="project-description">
             <h4>Project Lasso (Personal Lab Environments)</h4>
             <div className="subtitle">UX Design & Development</div>
             A productized, self-paced implementation of the Developer Enablement Program.
-            <br />
-            <a href={images[2].link}>
-              <img id="clickable-image" src={OnDemandLearning.src} alt="ODL"/>
-            </a>
+          </div>
+          <a href={images[2].link}>
+            <img id="clickable-image" src={OnDemandLearning.src} alt="ODL"/>
+          </a>
+          <div className="project-description">
             <h4>On-Demand Learning</h4>
             <div className="subtitle">UX Design & Development</div>
             A hands-on learning solution for Adobe Experience Platform.
-            <br />
           </div>
-          <div className="column">
-            <a href={images[1].link}>
-              <img id="clickable-image" src={Sandbox.src} alt="Sandbox copy"/>
-            </a>
+        </div>
+        <div className="column">
+          <a href={images[1].link}>
+            <img id="clickable-image" src={Sandbox.src} alt="Sandbox copy"/>
+          </a>
+          <div className="project-description">
             <h4>Sandbox Management App</h4>
             <div className="subtitle">UX Design & Development</div>
             A streamlined solution for transferring and syncing sandbox objects.
-            <br />
-            <a href={images[5].link}>
-              <img id="clickable-image" src={Disney.src} alt="Disney"/>
-            </a>
+          </div>
+          <a href={images[5].link}>
+            <img id="clickable-image" src={Disney.src} alt="Disney"/>
+          </a>
+          <div className="project-description">
             <h4>Disney Imagineering</h4>
             <div className="subtitle">3D Modeling, Graphic Design, Illustration, & Branding</div>
             A theme park concept that was a finalist in the 2018 Disney Imaginations competition.
-            <br />
           </div>
         </div>
-        {/*<div className="two-column-container">*/}
-        {/*  <div className="column">*/}
-        {/*    <a href={images[4].link}>*/}
-        {/*      <img id="clickable-image" src={NYT.src} alt="NYT"/>*/}
-        {/*    </a>*/}
-        {/*  </div>*/}
-        {/*  <div className="project-description-column">*/}
-        {/*    <h4>The New York Times</h4>*/}
-        {/*    <div className="subtitle">3D Modeling, Motion Graphics, Infographics, UI/UX Design & Development</div>*/}
-        {/*    Graphics I designed during my summer internship on the Immersive Storytelling team.*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </section>
-    );
+      </div>
+      {/*<div className="two-column-container">*/}
+      {/*  <div className="column">*/}
+      {/*    <a href={images[4].link}>*/}
+      {/*      <img id="clickable-image" src={NYT.src} alt="NYT"/>*/}
+      {/*    </a>*/}
+      {/*  </div>*/}
+      {/*  <div className="project-description-column">*/}
+      {/*    <h4>The New York Times</h4>*/}
+      {/*    <div className="subtitle">3D Modeling, Motion Graphics, Infographics, UI/UX Design & Development</div>*/}
+      {/*    Graphics I designed during my summer internship on the Immersive Storytelling team.*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+    </section>
+  );
 }
